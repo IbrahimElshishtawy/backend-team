8143905a8d8a4c7607c4cbec20429c244a706be9
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-02-26 14:45:56 +0200

    `Updated Node.js app to listen on port 3400 and updated index.html to change language to Arabic and add a new layout and content`

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -1,11 +1,11 @@
  const express = require('express')
 const app = express()
-const port = 3000
+const port = 3400
 
 app.get('/', (req, res) => {
   res.sendFile("./view/index.html" , { root: __dirname })
 })
 
 app.listen(port, () => {
-  console.log(`http://localhost:3000/`)
+  console.log(`http://localhost:${port}/`)
 })
