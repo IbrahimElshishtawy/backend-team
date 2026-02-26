59980989671bc1702ee80b8adf5930e693c3f410
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-02-26 14:42:36 +0200

    `Added Express.js server to serve index.html file on port 3000`

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -1,1 +1,11 @@
- 
+ const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile("./view/index.html" , { root: __dirname })
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:3000/`)
+})
